5711d81f860a3ae175f17c2edc3ff5a1b76ca3ee
jmbc <jmbc.soft@gmail.com>
2025-07-20 17:46:31 +0200

    Delete src/app directory

--- src/app/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8090',
-  googleClientId: '328681200787-bfr523r6ettgr11ir0r87kjag0tmlkcu.apps.googleusercontent.com'
-};
